c4200bef6f2be3afbd233fe3dbdf5b37b5d50596
1eedaegon <d8726243@gmail.com>
2021-03-11 19:25:14 +0900

    feat: use axios hook
    axios를 사용해서 data 비동기 호출

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { useEffect, useRef, useState } from "react"
+import useAxios from "./useAxios"
 
 
 
 const App = () => {
   // States and Hooks
-
+  const { loading, error, data, refetch } = useAxios({ url: "https://yts.mx/api/v2/list_movies.json" })
+  console.log(loading, error, data)
   // UI Components
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <h1>hi</h1>
+      <h1>{data && data.status}</h1>
+      <h2>{loading ? "Now loading..." : "Loaded!"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div >
   )
 }
